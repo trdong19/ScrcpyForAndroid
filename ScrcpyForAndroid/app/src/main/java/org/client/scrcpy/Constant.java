2bd9f86ef576338c709229fbbd02379cb9700d36
咚东 <84180727+trdong19@users.noreply.github.com>
2026-05-28 13:30:22 +0800

    添加新的屏幕控制参数常量
    - 添加PREF_TURN_SCREEN_OFF常量
    - 添加PREF_TURN_SCREEN_OFF_ON_CLOSE常量
    - 添加PREF_CUSTOM_ARGS常量

---
+++ ScrcpyForAndroid/app/src/main/java/org/client/scrcpy/Constant.java
@@ -0,0 +1,40 @@
+package org.client.scrcpy;
+
+
+public class Constant {
+    public static final String CONTROL_NAV = "control_nav";
+    public static final String CONTROL_NO = "no_control";
+    public static final String CONTROL_REMOTE_ADDR = "control_remote_addr";
+
+    public static final String PREFERENCE_SPINNER_RESOLUTION = "spinner_resolution";
+    public static final String PREFERENCE_SPINNER_BITRATE = "spinner_bitrate";
+
+    public static final String PREFERENCE_SPINNER_DELAY = "delay_control";
+
+    public static final String HISTORY_LIST_KEY = "history_list_key";
+    public static final String USER_ID = "user_id";
+
+    public static final String PREFERENCE_NAME = "scrcpy_prefs";
+
+    public static final String PREF_VIDEO_RESOLUTION = "video_resolution";
+    public static final String PREF_VIDEO_BITRATE = "video_bitrate";
+    public static final String PREF_VIDEO_FPS = "video_fps";
+    public static final String PREF_VIDEO_CODEC = "video_codec";
+    public static final String PREF_NO_VIDEO = "no_video";
+
+    public static final String PREF_AUDIO_ENABLE = "audio_enable";
+    public static final String PREF_AUDIO_CODEC = "audio_codec";
+    public static final String PREF_AUDIO_BITRATE = "audio_bitrate";
+
+    public static final String PREF_DISPLAY = "display";
+    public static final String PREF_ROTATION = "rotation";
+    public static final String PREF_SHOW_TOUCHES = "show_touches";
+    public static final String PREF_STAY_AWAKE = "stay_awake";
+    public static final String PREF_TURN_SCREEN_OFF = "turn_screen_off";
+    public static final String PREF_TURN_SCREEN_OFF_ON_CLOSE = "turn_screen_off_on_close";
+    public static final String PREF_CUSTOM_ARGS = "custom_args";
+
+    public static final String PREF_RECORD_ENABLE = "record_enable";
+    public static final String PREF_RECORD_FORMAT = "record_format";
+    public static final String PREF_RECORD_PATH = "record_path";
+}
